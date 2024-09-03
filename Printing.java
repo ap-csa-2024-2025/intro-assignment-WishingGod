a041a495c1f306827f94b8d87c206d03ccf4e781
WishingGod <hopeishere112@gmail.com>
2024-09-03 10:10:39 -0700

    commit

---
+++ Printing.java
@@ -0,0 +1,19 @@
+public class Printing {
+    
+    
+    public static void main(String[] args) {
+        int myAge = 24;
+        double price = 5.49;
+        
+        boolean isGood = false;
+
+        String name = "Jeffery Flags";
+
+        System.out.println("Hello my name is " + name + " and I am " + myAge + " years old.");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+
+
+}
